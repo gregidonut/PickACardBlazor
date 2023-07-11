78d63b8e9eec75c410ea520c9b6ad9166a6143d3
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-07-12 07:38:44 +0800

    feat: add CardPicker class from previous project
    added CardPicker app by using the add existing item option from Rider,
    
    now that's something neat, feels like bloat but I should probably do
    this more when writing using other languages as well

---
+++ PickACardBlazor/CardPicker.cs
@@ -0,0 +1,46 @@
+namespace PickRandomCards;
+
+public class CardPicker
+{
+    public static string[] PickSomeCards(int numberOfCards)
+    {
+        var pickedCards = new string[numberOfCards];
+
+        for (var i = 0; i < numberOfCards; i++)
+            pickedCards[i] = RandomValue() + " of " + RandomSuit();
+
+        return pickedCards;
+    }
+
+    private static string RandomSuit()
+    {
+        var suits = new[]
+        {
+            "Spades",
+            "Diamonds",
+            "Hearts",
+            "Clover"
+        };
+
+        var rnd = new Random();
+
+        return suits[rnd.Next(suits.Length)];
+    }
+
+    private static string RandomValue()
+    {
+        var values = new List<string>
+        {
+            "Ace",
+            "Jack",
+            "Queen",
+            "King"
+        };
+
+        for (var i = 2; i < 9; i++) values.Add(i.ToString());
+
+        var rnd = new Random();
+
+        return values[rnd.Next(values.Count)];
+    }
+}
